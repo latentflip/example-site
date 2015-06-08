205ebd1cbeb6c19feb2dcd47d8175d2dbdbf55c2
Philip Roberts <phil@latentflip.com>
2015-06-08 22:59:29 +0100

    Fix paths the way adam likes em, add browserify + uglify, document gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,18 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jade');
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
+
     grunt.initConfig({
+        //- compile main stylus file to output css file
         stylus: {
             compile: {
                 options: {
@@ -17,10 +23,13 @@ module.exports = function(grunt) {
                 },
 
                 files: {
-                    'public/css/main.css': ['styl/main.styl']
+                    'public/css/main.css': ['_styl/main.styl']
                 }
             }
         },
+
+        //- minify everything in the css output folder
+        //- just add .min.css to the filename
         cssmin: {
             target: {
                 files: [{
@@ -32,26 +41,72 @@ module.exports = function(grunt) {
                 }]
             }
         },
+
+        //- compile jade in the jade dir to matching paths in
+        //  the output dir
+        //- if you want pretty urls, make sure your jade files named index.jade, e.g:
+        //
+        //    about/index.jade   -> available at about/
+        //    contact/index.jade -> available at contact/
         jade: {
             target: {
               files: [{
                   expand: true,
-                  cwd: 'jade',
+                  cwd: '_jade',
                   src: ['**/*.jade'],
                   dest: 'public',
-                  ext: '.html'
+                  ext: '.html',
               }]
             }
         },
+
+        //- browserify the main javascript file to the output js dir
+        browserify: {
+            main: {
+                src: '_js/main.js',
+                dest: 'public/js/main.js'
+            }
+        },
+
+        //- minify any js files in the output dir
+        uglify: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/js',
+                    src: ['*.js', '!*.min.js'],
+                    dest: 'public/js',
+                    ext: '.min.js'
+                }]
+          }
+        },
+
+        //- copy any files in assets to the same place in output dir
+        //- beware of clobbering other generated files
+        copy: {
+            public: {
+                files: [
+                    {
+                        cwd: '_assets',
+                        expand: true,
+                        src: ['**'],
+                        dest: 'public/'
+                    },
+                ]
+            }
+        },
+
         watch: {
             build: {
-                files: ['styl/**', 'jade/**', 'assets/**'],
+                files: ['_styl/**', '_jade/**', '_js/**', 'assets/**'],
                 tasks: ['build'],
                 options: {
-                    livereload: true
+                    livereload: true,
+                    atBegin: true
                 }
             }
         },
+
         connect: {
             server: {
                 options: {
@@ -62,21 +117,10 @@ module.exports = function(grunt) {
                 }
             }
         },
-        copy: {
-            public: {
-                files: [
-                    {
-                        cwd: 'assets',
-                        expand: true,
-                        src: ['**'],
-                        dest: 'public/'
-                    },
-                ]
-            }
-        }
+
     });
 
-    grunt.registerTask('build', ['copy', 'stylus', 'cssmin', 'jade'])
+    grunt.registerTask('build', ['copy', 'stylus', 'cssmin', 'jade', 'browserify', 'uglify'])
     grunt.registerTask('serve', ['connect:server', 'watch'])
     grunt.registerTask('default', ['build'])
 };
---
+++ _js/main.js
@@ -0,0 +1,5 @@
+var url = require('url');
+
+console.log(url.parse(window.location.toString()));
+console.log('JS has been browserified');
+
